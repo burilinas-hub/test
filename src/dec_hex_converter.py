d049b0431830599a98f1ec11a4c7b0b303ae8f8a
Alex <burilinas@gmail.com>
2026-07-09 00:03:48 +0700

    commit2

--- src/dec_hex_converter.py
+++ src/dec_hex_converter.py
@@ -1,23 +1,18 @@
-a: int = 3167940560363555651584
-b: int = 3
-result = a * b
-# Выводим результат
-print(int(result))
+# a: int = 3167940560363555651584
+# b: int = 3
+# result = a * b
+# print(int(result))
 
 # Запрашиваем число у пользователя
 decimal_number = int(input("Введите число в десятичной системе: "))
 # Переводим число из dec в hex
 hex_number = hex(decimal_number)
-# Выводим результат
 print(f"Число {decimal_number} в шестнадцатеричной системе: {hex_number}")
+print(id(decimal_number))
 
 # Запрашиваем у пользователя ввод hex-числа
 hex_number = input("Введите число в шестнадцатеричной системе: ")
-
-try:
-    # Переводим из hex в decimal с помощью функции int
-    decimal_number = int(hex_number, 16)
-    print(f"В десятичной системе: {decimal_number}")
-except ValueError:
-    print("Ошибка: введено некорректное hexadecimal число.")
-
+# Переводим из hex в decimal с помощью функции int
+decimal_number = int(hex_number, 16)
+print(f"Число {hex_number} в десятичной системе: {decimal_number}")
+print(id(hex_number))
